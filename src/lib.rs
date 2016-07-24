a9d352a50950c5838e0c696cf08b4d0606d152d3
John Murray <me@johnmurray.io>
2016-07-24 22:14:10 +0000

    added more message types

--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,68 @@ mod tests {
 }
 
 
+/*
+ * EVENT OBJECT
+ */
+#[derive(RustcDecodable, RustcEncodable)]
+struct ChannelEventObject {
+    id: String,
+    name: String,
+    created: u32,
+    creator: String,
+}
+
+
+/*
+ * EVENTS
+ */
 
 #[derive(RustcDecodable, RustcEncodable)]
-struct Message {
+struct HelloEvent { 
+}
+#[derive(RustcDecodable, RustcEncodable)]
+struct UserTypingEvent {
     channel: String,
     user: String,
-    text: String,
+}
+/// Sent to all connections of a User when the read-cursor is advanced
+/// _Useful if multiple instances of a user connected at once._
+#[derive(RustcDecodable, RustcEncodable)]
+struct ChannelMarkedEvent {
+    channel: String,
     ts: u32,
+}
+#[derive(RustcDecodable, RustcEncodable)]
+struct ChannelCreatedEvent {
+    channel: ChannelEventObject,
+}
+/// Sent to all connections of a User when a channel is joined.
+#[derive(RustcDecodable, RustcEncodable)]
+struct ChannelJoinedEvent {
+    channel: ChannelEventObject,
+}
+/// Sent to all connections of a User when a channel is left
+#[derive(RustcDecodable, RustcEncodable)]
+struct ChannelLeftEvent {
+    channel: String,
+}
+#[derive(RustcDecodable, RustcEncodable)]
+struct ChannelDeletedEvent {
+    channel: String,
+}
+
+mod message_event {
+    /// The basic message that has no sub-type
+    #[derive(RustcDecodable, RustcEncodable)]
+    struct Basic {
+        channel: String,
+        user: String,
+        text: String,
+        ts: u32,
+    }
+
+    /// Sent to all existing members of a channel when someone new joins
+    type ChannelJoinEvent = Basic;
+    /// Sent to all existing members of a channel when someone leaves
+    type ChannelLeaveEvent = Basic;
 }
